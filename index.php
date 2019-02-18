396ec83cecdd42d4095461635971d1b89894601b
griff9214 <37296587+griff9214@users.noreply.github.com>
2019-02-19 00:12:34 +0300

    1

--- index.php
+++ index.php
@@ -71,14 +71,19 @@ $client = new Client();
 //    'reg' => [$car->region],
 //    'sts' => [$car->stsNumber],
 //]);
-//$response =  '{"error":0,"key":"8f0deda3a6bb20f7815831e081e81f4b"}';//$res->getBody();
 
-//$secureKey = "906bd6add30ec7eabcda22985ef6123a"; //json_decode($response, true)['key'];
+//$response = $res->getBody();
+$response =  '{"error":0,"key":"8f0deda3a6bb20f7815831e081e81f4b"}';
+
+//$secureKey = json_decode($response, true)['key'];
+$secureKey = "906bd6add30ec7eabcda22985ef6123a";
+
 
 //$res = $client->request('GET', 'https://shtrafyonline.ru/ajax/fines_gibdd_queue_result', [
 //    'query' => ['key' => $secureKey],
 //]);
-//echo $res->getBody();
+
+//$finesResponse = $res->getBody();
 $finesResponse = '{"error":0,"state":1,"data":{"error":0,"finesList":[{"koapSt":"","koapText":"\u041d\u0430\u0440\u0443\u0448\u0435\u043d\u0438\u0435 \u043f\u0440\u0430\u0432\u0438\u043b \u0440\u0430\u0441\u043f\u043e\u043b\u043e\u0436\u0435\u043d\u0438\u044f \u0442\u0440\u0430\u043d\u0441\u043f\u043e\u0440\u0442\u043d\u043e\u0433\u043e \u0441\u0440\u0435\u0434\u0441\u0442\u0432\u0430\r\n\r\n\u041c\u0435\u0441\u0442\u043e \u043d\u0430\u0440\u0443\u0448\u0435\u043d\u0438\u044f:\r\n\u041c\u041e\u0421\u041a\u0412\u0410 \u0413. \u041c\u041a\u0410\u0414, 58 \u041a\u041c 950 \u041c, \u041f-\u041e\u041f\u041e\u0420\u0410, \u0412\u041d\u0423\u0422\u0420\u0415\u041d\u041d\u042f\u042f \u0421\u0422\u041e\u0420\u041e\u041d\u0410\r\n\r\n\u041c\u0430\u0440\u043a\u0430 \u0430\u0432\u0442\u043e \u043d\u0430\u0440\u0443\u0448\u0438\u0442\u0435\u043b\u044f:\r\n\u0428\u041a\u041e\u0414\u0410 \u041e\u041a\u0422\u0410\u0412\u0418\u042f\r\n\r\n","fineDate":"2018-11-28","sum":"1500","billId":"18810177181128202096","hasDiscount":0,"hasPhoto":1,"divId":"1145000","discountSum":"0","discountUntil":"0000-00-00"}],"count":1,"inGarage":0},"params":{"num":"\u0415414\u0415\u0412","reg":"799","sts":"7756261168"},"type":0}';
 
 print_r(json_decode($finesResponse, true)["data"]["finesList"]);
