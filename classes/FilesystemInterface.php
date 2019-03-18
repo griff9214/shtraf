1bc7e24e2454d1aa375b559eaa303e8776f17839
griff9214 <37296587+griff9214@users.noreply.github.com>
2019-03-18 22:40:30 +0300

    filesys interface

---
+++ classes/FilesystemInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Григорий
+ * Date: 18.03.2019
+ * Time: 22:38
+ */
+
+namespace classes;
+
+
+interface FilesystemInterface
+{
+    public function makeDir($dirname)
+    {
+    }
+
+}
